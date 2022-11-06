32959095e5d7a7aa1248d910e71139d13299450d
ammrshmbng <ammarmare01@gmail.com>
2022-11-06 14:50:51 +0700

    stream on terminal

--- nodejs-core-modules/process/script.js
+++ nodejs-core-modules/process/script.js
@@ -1,11 +1,31 @@
+const quetions =[
+    "what is your name?",
+    "what is your fav hobby?",
+    "what is your preferred programming language?"
+]
 
-process.stdout.write('tes \n'); // it's the same as console.log();
 
-process.on('exit', function(code) {});  
-process.on('uncaughtException', function(err) {});
+const answers =[];
 
-console.log(process.argv)
-console.log(process.argv[0])
-console.log(process.argv[1])
+function ask(i){
+    process.stdout.write(`\n\n\n\n ${quetions[i]}`)
+    process.stdout.write("  >  ");
+}
 
-console.log(process.stdin);
+process.stdin.on('datas',function(data){
+    // process.stdout.write(data.toString().trim() + '\n');
+
+    answers.push(data.toString().trim());
+    if(answers.length < quetions.length){
+        ask(answers.length)
+    }else{
+        process.exit();
+    }
+});
+
+process.on('exit',function(){
+    process.stdout.write('\n\n\n\n');
+    process.stdout.write(`Go ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} later`)
+})
+
+ask(0);
